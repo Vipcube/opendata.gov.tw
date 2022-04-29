50bc93e19b2bd13d277eb7884858d1fb2a042b78
Bread God <Vipcube@users.noreply.github.com>
2022-04-29 23:08:54 +0800

    Finished test and result json.

--- src/test/java/org/vipcibe/opendata/gov/tw/nhi/RapidTestCsvProcess.java
+++ src/test/java/org/vipcibe/opendata/gov/tw/nhi/RapidTestCsvProcess.java
@@ -2,12 +2,20 @@ package org.vipcibe.opendata.gov.tw.nhi;
 
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.io.file.PathUtil;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.PrecisionModel;
+import org.vipcibe.opendata.gov.tw.json.geojson.Feature;
+import org.vipcibe.opendata.gov.tw.json.geojson.FeatureCollection;
+import org.vipcibe.opendata.gov.tw.nhi.model.RapidTestRecord;
 
 import java.io.IOException;
 import java.net.URI;
@@ -18,12 +26,21 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 @TestMethodOrder( MethodOrderer.OrderAnnotation.class )
 public class RapidTestCsvProcess {
 	private static HttpClient httpClient;
+	private static GeometryFactory geometryFactory;
+	private static ObjectMapper objectMapper;
 	private final static Path root = Paths.get( "src/test/resources" );
 	private final static Path rapidTestCsvPath = root.resolve( "raw/A21030000I-D03001-001.csv" );
+	private final static Path rapidTestJsonPath = root.resolve( "json/nhi/rapidTestStock.json" );
 
 	@BeforeAll
 	public static void setUp(){
@@ -31,6 +48,11 @@ public class RapidTestCsvProcess {
 				.version( HttpClient.Version.HTTP_1_1 )
 				.connectTimeout( Duration.ofSeconds( 10 ) )
 				.build();
+
+		geometryFactory = new GeometryFactory( new PrecisionModel( 10000 ), 4326 );
+
+		objectMapper = new ObjectMapper();
+		objectMapper.findAndRegisterModules();
 	}
 
 	@Test
@@ -51,7 +73,44 @@ public class RapidTestCsvProcess {
 
 	@Test
 	@Order( 2 )
-	public void transformRapidTestCSV(){
-		Assertions.assertTrue( PathUtil.exists( rapidTestCsvPath, false ) );
+	public void transformToJSON() throws JsonProcessingException {
+		if ( PathUtil.exists( rapidTestCsvPath, false ) ){
+			List<String> contents = FileUtil.readLines( rapidTestCsvPath.toFile(), StandardCharsets.UTF_8 );
+			var records = contents.stream()
+					.skip( 1 )
+					.map( line -> line.split( "," ) )
+					.map( splits -> RapidTestRecord.builder()
+							.id( splits[0] )
+							.name( splits[1] )
+							.address( splits[2] )
+							.lon( Double.parseDouble( splits[3] ) )
+							.lat( Double.parseDouble( splits[4] ) )
+							.phone( splits[5] )
+							.label( splits[6] )
+							.inStock( Integer.parseInt( splits[7] ) )
+							.updateTime( splits[8] )
+							.build() )
+					.collect( Collectors.toList());
+
+			var features = records.stream()
+					.map( record -> Feature.builder()
+							.properties( Map.of(
+									"id", record.getId(),
+									"name", record.getName(),
+									"address", record.getAddress(),
+									"phone", record.getPhone(),
+									"label", record.getLabel(),
+									"stock", record.getInStock(),
+									"updateTime", record.getUpdateTime()
+							) )
+							.geometry( geometryFactory.createPoint( new Coordinate( record.getLon(), record.getLat() ) ) )
+							.build() )
+					.collect( Collectors.toList());
+
+			FeatureCollection collection = FeatureCollection.builder()
+					.features( features )
+					.build();
+			FileUtil.writeString( objectMapper.writeValueAsString( collection ), rapidTestJsonPath.toFile(), StandardCharsets.UTF_8 );
+		}
 	}
 }
